635278cd7e5ad7311b73d8c0b1731fd2d7f39a91
Patrick <ledrap1992@gmail.com>
2022-12-14 13:28:18 +0100

    Add Load data logic

--- lib/data/habit_database.dart
+++ lib/data/habit_database.dart
@@ -1,3 +1,4 @@
+import 'package:habit_tracker_hive_local_storage/datetime/date_time.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 //reference our box
@@ -12,10 +13,27 @@ class HabitDatabase {
       ['Bieganie', false],
       ['Kodowanie', false],
     ];
+
+    _myBox.put('START_DATE', todaysDateFormatted());
   }
 
   //load data if it already exists
-  void loadData() {}
+  void loadData() {
+    //if it's a new day, get habit list from database
+    if (_myBox.get(todaysDateFormatted()) == null) {
+      todaysHabitList = _myBox.get('CuRRENT_HABIT_LIST');
+      //set all habits completed to false since it's a new day
+      for (int i = 0; i < todaysHabitList.length; i++) {
+        todaysHabitList[i][1] = false;
+      }
+    }
+
+    //if it's not a new day, load todays list
+    else {
+      todaysHabitList = _myBox.get(todaysDateFormatted());
+    }
+  }
+
   //update database
   void upadateDatabase() {}
 }
